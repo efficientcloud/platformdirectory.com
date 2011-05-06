54cb90f8d5c59f2c0d358cc2b626fcaa0bf56bd4
fin <fin+github@xbhd.org>
2011-05-06 17:27:59 +0200

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlatformdirectoryCom::Application.config.secret_token = '7150af7fda690724a77aa39248310ede0a8be2d20fe0630ec627f01ac113c6feb15fd63c442c582d7e58a994937872c41035b48a49976ad5fcfa686984d9a2a5'
